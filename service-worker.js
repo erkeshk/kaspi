f78bea5790bba976f6c95f83ed68064221e61f0f
Murat Kairbek <m_kairbek@outlook.com>
2025-01-16 20:23:06 +0500

    removed react native

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// service-worker.js
+
+const CACHE_NAME = "kaspi-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/static/js/main.js",
+  "/static/css/stylesheet.css",
+  "/kaspi-logo.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch Event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
